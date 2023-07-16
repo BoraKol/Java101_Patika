18b93b6e3856563c0ccd0abe9a61bb16415288a9
Bora Kol <83176378+BoraKol@users.noreply.github.com>
2023-07-16 22:33:03 +0300

    Add files via upload

---
+++ taksimetre/Taksimetre.java
@@ -0,0 +1,34 @@
+
+package taksimetre;
+
+import java.util.Scanner;
+
+public class Taksimetre {
+
+    public static void main(String[] args) {
+        int km;
+        double pricePerKm, totalPrice=10;
+        
+        Scanner scanner= new Scanner(System.in);
+        
+        System.out.println("Gidilecek mesafeyi km cinsinden söyleyiniz: ");
+        
+        km=scanner.nextInt();
+        
+        pricePerKm=(km * 2.2);
+        
+        totalPrice= 10 + pricePerKm;
+        
+        if(totalPrice<20) {
+            totalPrice=20;
+            System.out.println("Odemeniz gereken tutar "  + totalPrice + " Tl'dir" ) ;
+        }
+        
+        else {
+            System.out.println("Odemeniz gereken tutar " + totalPrice + " Tl'dir");
+        }
+        
+        
+    }
+    
+}
