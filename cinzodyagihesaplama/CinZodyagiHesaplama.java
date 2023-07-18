b26531437a753ba49754a92a478689c06f00449c
Bora Kol <83176378+BoraKol@users.noreply.github.com>
2023-07-18 18:05:22 +0300

    Update CinZodyagiHesaplama.java

--- cinzodyagihesaplama/CinZodyagiHesaplama.java
+++ cinzodyagihesaplama/CinZodyagiHesaplama.java
@@ -20,16 +20,58 @@ public class CinZodyagiHesaplama {
         System.out.println(reminder);
         
         switch(reminder){
+            
             case 0:
                 System.out.println("Cin Zodyagi Burcunuz: Maymun");
                 break;
+                
             case 1: 
                 System.out.println("Cin Zodyagi Burcunuz: Horoz");
                 break;
+            
+            case 2:
+                System.out.println("Cin Zodyagi Burcunuz: Kopek");
+                break;
+            
+            case 3:
+                System.out.println("Cin Zodyagi Burcunuz: Domuz");
+                break;
+            
+            case 4:
+                System.out.println("Cin Zodyagi Burcunuz: Fare");
+                break;
+               
+            case 5:
+                System.out.println("Cin Zodyagi Burcunuz: Okuz");
+                break;
                 
+            case 6:
+                System.out.println("Cin Zodyagi Burcunuz: Kaplan");
+                break;
+            
+            case 7:
+                System.out.println("Cin Zodyagi Burcunuz: Tavsan");
+                break;
+                
+            case 8:
+                System.out.println("Cin Zodyagi Burcunuz: Ejderha");
+                break;
+                
+            case 9:
+                System.out.println("Cin Zodyagi Burcunuz: Yilan");
+                break;
+                
+            case 10:
+                System.out.println("Cin Zodyagi Burcunuz: At");
+                break;
+                
+            case 11: 
+                System.out.println("Cin Zodyagi Burcunuz: Koyun");
+                break;
+              
         }
-        
         
     }
     
 }
+
